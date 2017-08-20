28bfb630e7e183fff1aea4a83cc2b93743c8df43
Personal <sam@intravenous.coffee>
2017-08-20 09:29:27 +0100

    removed serverside jwt shit

--- server.js
+++ server.js
@@ -16,20 +16,7 @@ mongoose.connect('mongodb://localhost/vue-pass');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.options('*', cors());
-
-// Middle ware
-router.use(function (req, res, next) {
-	if (req.headers.jwt !== "null" && req.headers.jwt !== undefined) {
-		if (!helper.validateJWT(req.headers.jwt)) {
-			res.status(401).send("Clear your cookies");
-			return;
-		}
-	} else if (req.path !== "/users") {
-		res.status(401).send('No JWT');
-		return;
-	}
-  	next();
-});
+//router.use(jwt.init('8A05954E4B3CF069A7DE0343ED697FB44CAA5A2E18468118C38766AAF8ABE32085F4DD425DFE44DB4FE323B39578BA3E098542FEE3E16FBA987DEFFDBC4FEE34', {}));
 
 router.route('/users')
 .post(function(req, res) {
@@ -53,9 +40,7 @@ router.route('/users')
 		user.validate(req, res)
 		.then(function(val){
 			if (typeof val == 'object') {
-				//res.status(200).send(val);
-				let newJWT = helper.createJWT(val[0]);
-				res.status(200).send(newJWT);
+				res.status(200).send(val);
 			} else {
 				res.status(401).send(val);
 			}
--- src/utilities/api.js
+++ src/utilities/api.js
@@ -4,14 +4,14 @@ const axios = require('axios');
 const hitApi = {
 	post: function(path, data) {
         return new Promise ((resolve, reject) => {
-            let JWT = cookies.read("JWT");
-            if (JWT == null) JWT = false;
+            // let JWT = cookies.read("JWT");
+            // if (JWT == null) JWT = false;
             axios.post(path, data,
             {headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "JWT": JWT ? JWT : null
+                "Content-Type": "application/x-www-form-urlencoded"
             }},)
             .then(function(res) {
+                console.log(res);
                 resolve(res);
             })
             .catch(function (error) {
